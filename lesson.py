a4ee6a0a69b8ad256f16f08f1e32046ba7531bee
Nganizi <dannganizip@gmail.com>
2022-08-18 16:16:19 +0300

    codes

---
+++ lesson.py
@@ -0,0 +1,34 @@
+print("hello world")
+
+# python code structure
+
+# variables
+my name = "Nganizi"
+my name is ="Nganizi"
+
+
+#Data types
+# Strings
+all chars = "A,B,C,D....,Z"
+my name = "Nganizi"
+
+
+# Numbers/Intergers
+my age = 12
+all numbers = 1,2,3,4,5,6....
+
+# Floating
+my weight = 12.6
+
+# Boolean True/False
+is raining = True
+
+
+
+my_name = "Nganizi"
+my_age = 23
+my_weight = 4.5
+
+# My name is Nganizi, I am 23 years and I weigh 4.5kgs
+print("My name is" + my_name +", I am" + str(my_age) + "years old")
+

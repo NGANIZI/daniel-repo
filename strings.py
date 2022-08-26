fff4b09265795507e242d0880d882f45fe2845ca
Nganizi <dannganizip@gmail.com>
2022-08-26 17:33:33 +0300

    new

---
+++ strings.py
@@ -0,0 +1,21 @@
+my_string = "I love python"
+print(my_string)
+
+
+# Indexing a string
+for i in range(len(my_string)):
+    print(my_string[i])
+
+print(my_string[3])
+
+# Slicing a string
+word = "python"
+
+print(word[0:3])
+
+#on
+print(word[-1:2])
+
+# deleting
+my_new_string = "UGANDA"
+del my_new_string[2]
